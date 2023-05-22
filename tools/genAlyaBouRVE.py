45da29cf0ad2fb9eb9d858b9d4abe2c61d9d657c
gguillam <gerard.guillamet@bsc.es>
2023-05-22 19:59:37 +0200

    Issue #13 adding some useful scripts

--- tools/genAlyaBouRVE.py
+++ tools/genAlyaBouRVE.py
@@ -1,34 +1,98 @@
-#!/usr/bin/python
 
 import sys
 import numpy
+
+from opeAlyaRVE import loadAlyaGeoDat
+from opeAlyaRVE import getRVEdimensionsAndCentering
+from opeAlyaRVE import getRVEnodesBoundaries
 
 """
 This script generates the list of external boundaries in Alya format from a brick-shaped RVE.
 We assume that it's corners are at (0,0,0) and (lx,ly,lz). The dimensions are determined from the nodal coordinates.
 """
 
-# processing command line arguments, get the
-# jobname
-if len(sys.argv)>1:
-    print( "Using file:",sys.argv[1])
-    source = sys.argv[1]
-else:
-    print( "Specify mesh file")
-    quit()
-
-def array_to_dict_AlyaNodDat(array):
+def getRVEelementBoundaries(ne):
     """
-    Input    : array : A 2D-dimensional array
-    Output   : A dictionary considering in which the key is the first item of the
-               row array
-    Example  : I = [[1,2,3],[4,5,6],[7,8,9]] -> O = {1 : [2,3], 4 : [5,6], 7 : [8,9]}
+    Get RVE element list for each boundary (face)
     """
-    r = dict()
-    for x in array:
-        r[int(x[0])] = numpy.array(x[1:])
-    return r
+    # List of elements belonging to the boundary elemetns
+    e1list = [] # X = 0
+    e2list = [] # X = L
+    e3list = [] # Y = 0
+    e4list = [] # Y = L
+    e5list = [] # Z = 0
+    e6list = [] # Z = L
+    for i in range(len(ne)):
+        ielem = ne[i][0]
+        elcon = ne[i][1:]
+        for inode in elcon:
+            if inode in x0:
+                e1list.append(ielem)
+            elif inode in xl:
+                e2list.append(ielem)
+            elif inode in y0:
+                e3list.append(ielem)
+            elif inode in yl:
+                e4list.append(ielem)
+            elif inode in z0:
+                e5list.append(ielem)
+            elif inode in zl:
+                e6list.append(ielem)        
 
+    # Remove repeated nodes for each list
+    e1list = list(set(e1list))
+    e2list = list(set(e2list))
+    e3list = list(set(e3list))
+    e4list = list(set(e4list))
+    e5list = list(set(e5list))
+    e6list = list(set(e6list))
+    return e1list, e2list, e3list, e4list, e5list, e6list
+    
+def getRVEboundaries(ne,blist):
+    """
+    Get RVE boundaries for each ID.
+    """
+    #
+    # Defining possible face connectivities
+    #
+    surfhex08 = {'s1' : [4, 3, 2, 1],
+                 's2' : [5, 6, 7, 8],
+                 's3' : [2, 3, 7, 6],
+                 's4' : [1, 5, 8, 4],
+                 's5' : [3, 4, 8, 7],
+                 's6' : [1, 2, 6, 5]}
+    surfpen06 = {'s1' : [3, 2, 1],
+                 's2' : [5, 6, 4],
+                 's3' : [2, 5, 4, 1],
+                 's4' : [3, 6, 5, 2],
+                 's5' : [1, 4, 6, 3]}
+    b_list = []
+    nboun = 0
+    for iboun in range(len(blist)):
+        elist       = blist[iboun][0]
+        nodes_plane = blist[iboun][1]
+        b = []
+        for ielem in elist:
+            # Line element connectivity: element_id node1, node2, node3
+            e = ne[ielem-1]
+            if len(e) == 7:
+                surfelem = surfpen06
+            elif len(e) == 9:
+                surfelem = surfhex08
+            else:
+                print('Element type not found!')
+                exit()
+            for s, c in surfelem.items():
+                bcon = list([i for i in e[c]])
+                iBelongToBoundary = all(inode in nodes_plane for inode in bcon)
+                if iBelongToBoundary:
+                    nboun += 1
+                    bcon = bcon + [ielem]
+                    b.append(bcon)
+        b_list.append(b)
+        
+    return b_list, nboun
+    
 def writeAlyaBouDat(blist):
     """
     Write Alya boundar file
@@ -66,103 +130,43 @@ def writeAlyaFixDat(blist):
             fo.write("{0} {1}\n".format(iboun,icode))
     fo.write("END_ON_BOUNDARIES\n")
     fo.close()
+
+if __name__ == '__main__':
+
+    # processing command line arguments, get the
+    # jobname
+    if len(sys.argv)>1:
+        print( "Using file:",sys.argv[1])
+        source = sys.argv[1]
+    else:
+        print( "Specify mesh file")
+        quit()
+
+    # Mapping tolerance
+    tol = 1.0e-6
     
-# initializing
-# Mapping tolerance
-tol = 1.0e-6
+    # Load Alya geometry file   
+    ne, na = loadAlyaGeoDat(source)
+        
+    # Get dimensions of the RVE and center to 0,0,0 when necessary
+    na, n, x, y, z, lx, ly, lz = getRVEdimensionsAndCentering(na)
 
-# Load coordinates and element connectivity files 
-# Skip first and last rows
-na = numpy.loadtxt(open(f'{source}'+'.nod.dat').readlines()[:-1], skiprows=1, dtype=None)
-ne = numpy.genfromtxt(f'{source}'+'.ele.dat', skip_header=1, skip_footer=1, invalid_raise=False, loose=False, dtype="i8")
+    # Get node list from each boundary (RVE face)
+    x0, y0, z0, xl, yl, zl = getRVEnodesBoundaries(tol, n, x, y, z, lx, ly, lz)
 
-# Nodes in different lists
-n  = numpy.int_(na[:,0])
-x  = numpy.array(na[:,1]) 
-y  = numpy.array(na[:,2])
-z  = numpy.array(na[:,3])
+    # Get element list from each boundary (RVE face)
+    e1list, e2list, e3list, e4list, e5list, e6list = getRVEelementBoundaries(ne)
+    blist = [[e1list,x0], [e2list,xl], [e3list,y0], [e4list,yl], [e5list,z0], [e6list,zl]]
 
-# Determine dimensions of the brick and center when necessary
-[xmax,ymax,zmax]=[x.max(),y.max(),z.max()]
-[xmin,ymin,zmin]=[x.min(),y.min(),z.min()]
-lx = xmax-xmin
-ly = ymax-ymin
-lz = zmax-zmin
-print( "lx: {0}\nly: {1}\nlz: {2}".format(lx,ly,lz))
-x = x - xmin
-y = y - ymin
-z = z - zmin
-na[:,1] = x
-na[:,2] = y
-na[:,3] = z
+    # Built RVE element boundaries for each boundary
+    b_list, nboun = getRVEboundaries(ne,blist)
 
-# Nodes in dictionary 
-dnod = array_to_dict_AlyaNodDat(na)
+    # Write new files
+    print('Writting Alya jobName.bou.dat ...')
+    writeAlyaBouDat(b_list)
+    print('Writting Alya jobName.fix.dat ...')
+    writeAlyaFixDat(b_list)
 
-# Determine list of nodes for each limit bound
-x0 = numpy.extract(x <= tol, n)
-y0 = numpy.extract(y <= tol, n)
-z0 = numpy.extract(z <= tol, n)
-xl = numpy.extract(abs(x-lx) <= tol, n)
-yl = numpy.extract(abs(y-ly) <= tol, n)
-zl = numpy.extract(abs(z-lz) <= tol, n)
-
-#
-# Defining possible face connectivities
-#
-surfhex08 = {'s1' : [4,3,2,1],
-             's2' : [5,6,7,8],
-             's3' : [2,3,6,5],
-             's4' : [1,2,6,5],
-             's5' : [3,4,8,7],
-             's6' : [1,5,8,4]
-             }
-
-# List of elements belonging to the boundary elemetns
-e1list = [] # X = 0
-e2list = [] # X = L
-e3list = [] # Y = 0
-e4list = [] # Y = L
-e5list = [] # Z = 0
-e6list = [] # Z = L
-for i in range(len(ne)):
-    ielem = ne[i][0]
-    elcon = ne[i][1:]
-    for inode in elcon:
-        if inode in x0:
-            e1list.append(ielem)
-        elif inode in xl:
-            e2list.append(ielem)
-        elif inode in y0:
-            e3list.append(ielem)
-        elif inode in yl:
-            e4list.append(ielem)
-        elif inode in z0:
-            e5list.append(ielem)
-        elif inode in zl:
-            e6list.append(ielem)        
-
-# Remove repeated nodes for each list 
-e1list = list(set(e1list))
-e2list = list(set(e2list))
-e3list = list(set(e3list))
-e4list = list(set(e4list))
-e5list = list(set(e5list))
-e6list = list(set(e6list))
-
-
-b1,b2,b3,b4,b5,b6 = [],[],[],[],[],[]
-# Boundary 1 (X = 0)
-for ielem in e1list:
-    elcon = ne[ielem-1][1:]
-    current_b1 = list(elcon[:4]) + [ielem]
-    b1.append(current_b1)
-
-blist = [b1,b2,b3,b4,b5,b6]
-
-# Write new files
-writeAlyaBouDat(blist)
-writeAlyaFixDat(blist)
-
-#print(source+'.bou.dat file written!')
-#print(source+'.fix.dat file written!')
+    # Print total bounaries
+    print('Total boundaries created:',nboun)
+    
